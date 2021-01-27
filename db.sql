ea818cba56d9f780b4faac74bd52e59ec5cd3941
keanwc <keanwc@gmail.com>
2021-01-27 09:48:54 +0800

    removed files

--- db.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `student`;
-CREATE TABLE `student` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `age` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
